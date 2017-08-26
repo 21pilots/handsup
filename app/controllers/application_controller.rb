0c46f5577ccf9cee5a70eef9fd8b3e407fd3fab1
Zoe Park <souveniraile@gmail.com>
2017-08-26 08:12:42 +0000

    for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     protected
     
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :major])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :major])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :major_id])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :major_id])
     end
 end
